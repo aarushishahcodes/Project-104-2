25ce6ab7115b501065be95cd6e3904b34a95edfa
aarushishahcodes <96890538+aarushishahcodes@users.noreply.github.com>
2024-05-24 19:21:52 -0400

    Add files via upload

---
+++ Project104.py
@@ -0,0 +1,74 @@
+cv2.putText(img
+            "Sun",
+            (20,300)
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.5,
+            (225,255,255)
+            )
+
+cv2.putText(img
+            "Mercury",
+            (20,300)
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.5,
+            (270,270,270)
+            )
+
+cv2.putText(img
+            "Venus",
+            (20,300)
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.5,
+            (300,300,300)
+            )
+
+cv2.putText(img
+            "Earth",
+            (20,300)
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.5,
+            (340,340,340)
+            )
+
+cv2.putText(img
+            "Mars",
+            (20,300)
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.5,
+            (400,400,400)
+            )
+
+cv2.putText(img
+            "Jupiter",
+            (20,300)
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.5,
+            (450,450,450)
+            )
+
+cv2.putText(img
+            "Saturn",
+            (20,300)
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.5,
+            (500,500,500)
+            )
+
+cv2.putText(img
+            "Uranus",
+            (20,300)
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.5,
+            (550,550,550)
+            )
+
+cv2.putText(img
+
+            "Neptune",
+            (20,300)
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.5,
+            (600,600,600)
+            )
+
+cv2.imwrite("Solar_systemwithname.jpg",img)
